79b1010989a86b416ba4ae23f329bde7b553ef49
PatricioVill <patricio.vill74@gmail.com>
2021-06-23 18:50:26 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const path = require("path");
+const port = 3030;
+app.use(express.static("public"));
+
+app.listen(port,() => console.log("Server en puerto: " + port))
+app.get("/", (req,res) => res.sendFile(path.join(__dirname,"views","home.html")))
